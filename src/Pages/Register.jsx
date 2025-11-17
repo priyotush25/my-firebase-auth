7d94eb3c994bc5efb5d64b313e44915a66d07769
Priyotush <priyotushpaul40@gmail.com>
2025-11-17 19:43:50 +0600

    update profile

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,27 +1,39 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useState } from "react";
 import { FiEye, FiEyeOff } from "react-icons/fi";
-import Swal from "sweetalert2";
+import { toast } from "react-toastify";
+
 import { auth } from "../Firebase/firebase.config";
 
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [active, setActive] = useState(false);
+  const [name, setName] = useState("");
+  const [photo, setPhoto] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     // sign up in firebase
     createUserWithEmailAndPassword(auth, email, password)
-      .then(() => {
-        console.log("sign up successfully");
+      .then((res) => {
+        toast.success("signup successfully");
 
-        Swal.fire({
-          icon: "success",
-          text: "Registration successfully",
-        });
+        // update profile
+        updateProfile(res.user, {
+          displayName: name,
+          photoURL: photo,
+        })
+          .then((res) => {
+            console.log(res);
+          })
+          .catch((e) => {
+            console.log(e.message);
+          });
 
+        setName("");
+        setPhoto("");
         setEmail("");
         setPassword("");
       })
@@ -61,10 +73,7 @@ const Register = () => {
             msg = error.message;
         }
 
-        Swal.fire({
-          icon: "error",
-          text: msg,
-        });
+        toast.error("Signup Fail");
       });
   };
 
@@ -75,8 +84,34 @@ const Register = () => {
         onSubmit={handleSubmit}
       >
         <h1 className="text-4xl font-semibold text-center">Sing up</h1>
+
+        {/* name */}
         <div>
-          <p className="text-xl font-semibold mb-2">Email</p>
+          <p className="text-xl font-semibold mb-2">Name</p>
+          <input
+            type="text"
+            value={name}
+            placeholder="Enter your name"
+            onChange={(e) => setName(e.target.value)}
+            className="px-4 py-2 border-2 border-gray-500 w-full"
+          />
+        </div>
+
+        {/* photo url */}
+        <div>
+          <p className="text-xl font-semibold mb-2 mt-4">PhotURL</p>
+          <input
+            type="text"
+            value={photo}
+            placeholder="Enter your photo url"
+            onChange={(e) => setPhoto(e.target.value)}
+            className="px-4 py-2 border-2 border-gray-500 w-full"
+          />
+        </div>
+
+        {/* email */}
+        <div>
+          <p className="text-xl font-semibold mb-2 mt-4">Email</p>
           <input
             type="email"
             value={email}
@@ -85,6 +120,8 @@ const Register = () => {
             className="px-4 py-2 border-2 border-gray-500 w-full"
           />
         </div>
+
+        {/* password */}
         <div>
           <p className="text-xl font-semibold mb-2 mt-4">Password</p>
           <input
@@ -102,6 +139,7 @@ const Register = () => {
             {active ? <FiEye /> : <FiEyeOff />}
           </button>
         </div>
+
         <div>
           <button className="my-btn">Register</button>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
+import { ToastContainer } from "react-toastify";
 import "./index.css";
 import route from "./Route/route";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={route} />
+    <ToastContainer />
   </StrictMode>
 );
