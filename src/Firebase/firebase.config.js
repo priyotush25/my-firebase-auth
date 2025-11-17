4ae9cfe88b01160f05597fb4a9f58e1bd9141831
Priyotush <priyotushpaul40@gmail.com>
2025-11-17 19:17:44 +0600

    add env file

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC2c9KWM7qjEf4cPs4LHJvCpP_a3qEDWG8",
-  authDomain: "my-firebase-auth-67fb0.firebaseapp.com",
-  projectId: "my-firebase-auth-67fb0",
-  storageBucket: "my-firebase-auth-67fb0.firebasestorage.app",
-  messagingSenderId: "889937855634",
-  appId: "1:889937855634:web:88a15ec3c6d4ba30b1124b",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
